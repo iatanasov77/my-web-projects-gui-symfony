24b45c61d808950e7038a6d7d32a723c9ebc8d82
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-05-09 16:03:36 +0000

    Add PhpBrew Extension 'xdebug'

--- src/Component/PhpBrew.php
+++ src/Component/PhpBrew.php
@@ -5,7 +5,17 @@ class PhpBrew
     const EXTENSIONS   = [
         'MongoDB'   => 'mongodb', 
         'Cassandra' => 'cassandra',
+        'Xdebug'    => 'xdebug'
     ];
+    
+    /*
+     * XDEBUG 3 Add Configuration
+     * ========================
+     * xdebug.mode = debug
+     * xdebug.start_with_request = 1
+     * xdebug.discover_client_host = 1
+     * xdebug.client_port = 9000
+     */
     
     public static function extensions( $options = [] )
     {
