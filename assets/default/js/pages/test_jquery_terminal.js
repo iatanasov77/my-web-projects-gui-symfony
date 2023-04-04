d97f63fa6e4b594de47a182343fac0b0e59cbab1
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-04-04 14:11:56 +0000

    Testing JQuery Terminal.

--- assets/default/js/pages/test_jquery_terminal.js
+++ assets/default/js/pages/test_jquery_terminal.js
@@ -1,5 +1,3 @@
-import * as spinnerService from '../includes/spinner';
-
 import 'jquery.terminal/js/jquery.terminal.js';
 require( 'jquery.terminal/css/jquery.terminal.css' );
 
@@ -9,6 +7,9 @@ require( 'jquery.terminal/css/jquery.terminal.css' );
  * https://www.npmjs.com/package/jquery.terminal
  * https://terminal.jcubic.pl/
  * https://stackoverflow.com/questions/45975717/why-does-this-jqueryterminal-cmd-function-not-work
+ *
+ * https://leash.jcubic.pl/
+ * https://github.com/jcubic/leash/
  */
 
 function testGreetings()
@@ -60,93 +61,144 @@ function testCmd()
  *==============
  * https://terminal.jcubic.pl/examples.php#simple_ajax
  */
-function testAjax( form )
+function testAjax()
 {
-    var service = form.attr( 'action' );
-    var data    = form.serializeArray();
+    $( '#formProjectThirdPartyContainer' ).html( '<div class="console" id="consoleProjectInstall"><div id="projectInstallContainer"></div></div>' );
     
     $( '#projectInstallContainer' ).terminal( function( command, term ) {
-        term.pause();
-        $.post( service , data ).then( function( response ) {
-            term.echo( response ).resume();
+        var finish  = false;
+        var msg     = "Wait I'm executing ajax call";
+        
+        term.set_prompt( '> ' );
+        typed_message( term, msg, 200, function() {
+            finish = true;
+        });
+            
+        $.post( '/projects/test-terminal' ).then( function( response ) {
+            console.log( response );
+            //term.echo( response ).resume();
+            //term.echo( response );
+            
+            ( function wait() {
+                if ( finish ) {
+                    term.echo( response );
+                } else {
+                    setTimeout( wait, 500 );
+                }
+            })();
+            
         });
     }, {
-        greetings: 'Simple php example'
+        greetings: null,
+    });
+}
+
+function testPrompt()
+{
+    $( '#formProjectThirdPartyContainer' ).html( '<div class="console" id="consoleProjectInstall"><div id="projectInstallContainer"></div></div>' );
+
+    $( '#projectInstallContainer' ).terminal( function( cmd, term ) {
+        var finish = false;
+        var msg = "Wait I'm executing ajax call";
+        term.set_prompt( '> ' );
+        typed_message( term, msg, 200, function() {
+            finish = true;
+        });
+        var args = {command: cmd};
+        
+        
+        term.pause();
+        $.get( '/projects/test-terminal', args, function( result ) {
+            ( function wait() {
+                if ( finish ) {
+                    term.echo( result );
+                } else {
+                    setTimeout( wait, 500 );
+                }
+            })();
+        });
+        
+        
+    }, {
+        name: 'xxx',
+        greetings: null,
+        width: 500,
+        height: 300,
+        onInit: function( term ) {
+            // first question
+            var msg = "Wellcome to my terminal";
+            typed_message( term, msg, 200, function() {
+                typed_prompt( term, "what's your name:", 100 );
+            });
+        },
+        keydown: function(e) {
+            //disable keyboard when animating
+            if ( anim ) {
+                return false;
+            }
+        }
     });
 }
 
 $( function()
 {
-    /*
     $( '#btnTestJqueryTerminal' ).on( 'click', function( e )
     {
-        testGreetings();
+        //testGreetings();
         //testLogin();
         //testCmd();
+        testAjax();
+        //testPrompt();
     });
-    */
-    
-    $( '#btnTestJqueryTerminal' ).on( 'click', function( e )
-    {
-        $( '#formProjectThirdPartyContainer' ).html( spinnerService.spinner );
-        
-        $.ajax({
-            type: "GET",
-            url: "/third-party-projects/edit/0",
-            success: function( response )
-            {
-                $( '#formProjectThirdPartyContainer' ).html( response );
-            },
-            error: function()
-            {
-                alert( "SYSTEM ERROR!!!" );
-            }
-        });
-    });
-    
-    $( '#create-project-third-party-modal' ).on( 'click', '#btnInstallProjectThirdParty', function( e )
-    {
-        var form            = $( '#formProjectThirdParty' );
-        var predefinedType  = $( '#third_party_project_predefinedType' ).val();
-        
-        testAjax( form );
-    });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    $( '#formProjectThirdPartyContainer' ).on( 'change', '#third_party_project_predefinedType', function( e )
-    {
-        var type    = $( this ).val();
-        if ( predefinedProjects[type] == undefined ) {
-            alert( "Project type '" + type + "' is undefined!" );
-        }
-        
-//      $( '#project_sourceType' ).val( predefinedProjects[type].sourceType );
-//      $( '#project_repository' ).val( predefinedProjects[type].sourceUrl );
-//      $( '#project_branch' ).val( predefinedProjects[type].branch );
-        
-        $.ajax({
-            type: "GET",
-            url: '/predefined_project_form/' + type,
-            success: function( response )
-            {
-                $( '#predefinedProjectProperties' ).html( response );
-                
-                if ( type == 'presta_shop' ) {
-                    $( '#third_party_project_installType' ).val( 'composer' );
-                    $( '#formProjectThirdParty' ).attr( 'action', '/projects/third-party-install' );
-                }
-            },
-            error: function()
-            {
-                alert( "SYSTEM ERROR!!!" );
-            }
-        });
-    });
+
 });
+
+
+
+
+
+var anim = false;
+    
+function typed( finish_typing ) {
+    return function( term, message, delay, finish ) {
+        anim = true;
+        var prompt = term.get_prompt();
+        var c = 0;
+        if ( message.length > 0 ) {
+            term.set_prompt( '' );
+            var new_prompt = '';
+            var interval = setInterval( function() {
+                var chr = $.terminal.substring( message, c, c + 1 );
+                new_prompt += chr;
+                term.set_prompt( new_prompt );
+                c++;
+                if ( c == length( message ) ) {
+                    clearInterval( interval );
+                    // execute in next interval
+                    setTimeout( function() {
+                        // swap command with prompt
+                        finish_typing( term, message, prompt );
+                        anim = false
+                        finish && finish();
+                    }, delay);
+                }
+            }, delay);
+        }
+    };
+}
+
+function length( string ) {
+    string = $.terminal.strip( string );
+    return $( '<span>' + string + '</span>' ).text().length;
+}
+
+var typed_prompt = typed( function( term, message, prompt ) {
+    term.set_prompt( message + ' ' );
+});
+
+var typed_message = typed( function( term, message, prompt ) {
+    term.echo( message )
+    term.set_prompt( prompt );
+});
+    
+    
