9ac853b33092c07d3a50a44f4984b67dd0c84207
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-04-04 17:30:18 +0000

    Use JQuery Terminal For PhpBrew Installation Process.

--- assets/default/js/pages/php_versions.js
+++ assets/default/js/pages/php_versions.js
@@ -9,6 +9,62 @@ require( 'jquery-multi-select/src/jquery.multi-select.js' );
 require( 'tooltipster/dist/css/tooltipster.bundle.css' );
 require( 'tooltipster/dist/js/tooltipster.bundle.js' );
 
+import 'jquery.terminal/js/jquery.terminal.js';
+require( 'jquery.terminal/css/jquery.terminal.css' );
+
+function startInstall( url, serializedData )
+{
+    var data                = installData( serializedData );
+    
+    var lastResponseLength  = false;
+    $.ajax({
+        method: 'POST',
+        url: url,
+        data: serializedData,
+        xhrFields: {
+            // Getting on progress streaming response
+            onprogress: function( e )
+            {
+                var progressResponse;
+                var response    = e.currentTarget.response;
+                if( lastResponseLength === false ) {
+                    $( "#commandPhpVersion" ).html( response );
+                    
+                    progressResponse    = response;
+                    lastResponseLength  = response.length;
+                } else {
+                    progressResponse    = response.substring( lastResponseLength );
+                    lastResponseLength  = response.length;
+                }
+                
+                // In My Case
+                $( "#phpInstallContainer" ).append( progressResponse ).animate( {scrollTop: $( '#phpInstallContainer' ).prop( "scrollHeight" ) }, 0 );
+                
+                if ( progressResponse == '' ) {
+                    
+                }
+            }
+        }
+    })
+    .done( function( html ) {
+        var setupUrl     = "/php-versions/" + data.version + "/setup";
+        var startUrl     = "/php-versions/" + data.version + "/start-fpm";
+        
+        $.get( setupUrl, function( response ) {
+            $( "#phpInstallContainer" ).append( '<br><br>PhpFpm Setup is Done!<br>' ).animate( {scrollTop: $( '#phpInstallContainer' ).prop( "scrollHeight" ) }, 0 );
+            $.get( startUrl, function( response ) {
+                $( "#phpInstallContainer" ).append( 'PhpFpm is Started!<br>' ).animate( {scrollTop: $( '#phpInstallContainer' ).prop( "scrollHeight" ) }, 0 );
+            });
+        });
+        
+        $( "#btnClosePhpInstall" ).text( 'Close' );
+        $( "#btnClosePhpInstall" ).show();
+    })
+    .fail( function() {
+        alert( "AJAX return an ERROR !!!" );
+    });
+}
+
 function installPhpVersion( version, defaultVariants )
 {
 	$( "#lablePhpVersion" ).text( version );
@@ -34,7 +90,7 @@ function installData( serializedForm )
         
     };
     
-    for ( i = 0; i < serializedForm.length; i++ ) {
+    for ( var i = 0; i < serializedForm.length; i++ ) {
         switch ( serializedForm[i].name ) {
             case 'phpBrewVariants[]':
                 data.phpBrewVariants.push( serializedForm[i].value );
@@ -96,56 +152,13 @@ $( function()
 		$( "#formPhpInstall" ).hide( 1000 );
 		$( "#consolePhpInstall" ).show();
 		
-		var lastResponseLength	= false;
-		$.ajax({
-			method: 'POST',
-			url: url,
-			data: serializedData,
-			xhrFields: {
-	            // Getting on progress streaming response
-	            onprogress: function( e )
-	            {
-	                var progressResponse;
-	                var response	= e.currentTarget.response;
-	                if( lastResponseLength === false ) {
-	                	$( "#commandPhpVersion" ).html( response );
-	                	
-	                    progressResponse	= response;
-	                    lastResponseLength	= response.length;
-	                } else {
-	                    progressResponse	= response.substring( lastResponseLength );
-	                    lastResponseLength	= response.length;
-	                }
-	                
-	                // In My Case
-	                $( "#phpInstallContainer" ).append( progressResponse ).animate( {scrollTop: $( '#phpInstallContainer' ).prop( "scrollHeight" ) }, 0 );
-	                
-	                if ( progressResponse == '' ) {
-	                	
-	                }
-	            }
-	        }
-		})
-		.done( function( html ) {
-			var setupUrl     = "/php-versions/" + data.version + "/setup";
-			var startUrl     = "/php-versions/" + data.version + "/start-fpm";
-			
-			$.get( setupUrl, function( response ) {
-				$( "#phpInstallContainer" ).append( '<br><br>PhpFpm Setup is Done!<br>' ).animate( {scrollTop: $( '#phpInstallContainer' ).prop( "scrollHeight" ) }, 0 );
-				$.get( startUrl, function( response ) {
-					$( "#phpInstallContainer" ).append( 'PhpFpm is Started!<br>' ).animate( {scrollTop: $( '#phpInstallContainer' ).prop( "scrollHeight" ) }, 0 );
-				});
-			});
-			
-			$( "#btnClosePhpInstall" ).text( 'Close' );
-			$( "#btnClosePhpInstall" ).show();
-		})
-		.fail( function() {
-			alert( "AJAX return an ERROR !!!" );
-		});
+		var term    = $( '#phpInstallContainer' ).terminal({
+            install: function() {
+                startInstall( url, serializedData );
+            }
+        });
+        term.exec( 'install' );
 		
 		return false;
 	});
-	
-	
 });
