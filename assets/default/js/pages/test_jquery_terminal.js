cdeb594d3239053c22b09b4762e501e82e813fab
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-04-04 07:00:21 +0000

    Add JqueryTerminal to Test It.

--- assets/default/js/pages/test_jquery_terminal.js
+++ assets/default/js/pages/test_jquery_terminal.js
@@ -8,25 +8,53 @@ require( 'jquery.terminal/css/jquery.terminal.css' );
  * https://terminal.jcubic.pl/
  * https://stackoverflow.com/questions/45975717/why-does-this-jqueryterminal-cmd-function-not-work
  */
+
+function testGreetings()
+{
+    $( '#projectInstallContainer' ).terminal( function(command)
+    {
+        if ( command !== '' ) {
+            var result = window.eval( command );
+            if ( result != undefined ) {
+                this.echo( String( result ) );
+            }
+        }
+    }, {
+        greetings: 'Javascript Interpreter',
+        name: 'js_demo',
+        height: 200,
+        width: 450,
+        prompt: 'js> '
+    });
+}
+
+function testLogin()
+{
+    $( '#projectInstallContainer' ).terminal( '/projects/third-party-install', {login: true} );
+}
+
+function testCmd()
+{
+    $( '#projectInstallContainer' ).cmd({
+        prompt: '> ',
+        width: '100%',
+        keymap: function( command ) {
+        },
+        commands: function( command ) {
+            console.log( command );
+        }
+    });
+}
+
 $( function()
 {
     $( '#btnTestJqueryTerminal' ).on( 'click', function( e )
     {
         $( '#formProjectThirdPartyContainer' ).html( '<div class="console" id="consoleProjectInstall"><div id="projectInstallContainer"></div></div>' );
         
-        $( '#projectInstallContainer' ).terminal( '/projects/third-party-install', {login: true} );
-        
-        /*
-        $( '#projectInstallContainer' ).cmd({
-            prompt: '> ',
-            width: '100%',
-            keymap: function( command ) {
-            },
-            commands: function( command ) {
-                console.log( command );
-            }
-        });
-        */
+        testGreetings();
+        //testLogin();
+        //testCmd();
     });
     
     
